784c8cd438220c0118b0a74ec0947c2836618092
Testing wala User <apoorva3569@gmail.com>
2023-10-07 17:00:39 +0530

    Added contents on home page

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -5,17 +5,37 @@ export default function Home () {
         <main className = {s.home}>
             <div className = {s.cover}>
                 <div className = {s.content}>
-                    <h1>EMBRACE</h1><br />
-                    <h2>
-                        <span>E</span>nabling
-                        <span> M</span>ulti-disciplinary and
-                        <span> B</span>roader
-                        <span> R</span>adio
-                        <span> A</span>pplications for
-                        <span> C</span>ommunity
-                        <span> E</span>mpowerment
-                    </h2><br />
+                    <div>
+                        <h1>EMBRACE</h1><br />
+                        <h2>
+                            <span>E</span>nabling
+                            <span> M</span>ulti-disciplinary and
+                            <span> B</span>roader
+                            <span> R</span>adio
+                            <span> A</span>pplications for
+                            <span> C</span>ommunity
+                            <span> E</span>mpowerment
+                        </h2>
+                    </div>
                     {/* <h3>Empowering Communities through Disaster Resilience and Education</h3> */}
+                    <img src="/gifpic.gif" alt="" />
+                    
+                </div>
+            </div>
+            <div className={s.about}>
+                <div className={s.content}>
+                    <div>
+                        <h1>About Us</h1><br />
+                        <h2>Empowering Communities through Disaster Resilience and
+                            Education
+                        </h2><br />
+                        <p>EMBRACE aims to address several interconnected goals centred around
+                            amateur radio and its significance in disaster management, education,
+                            gender empowerment, promoting HAM radio and introducing innovative
+                            communication solutions in the coastal regions of the Karnataka state.
+                        </p>
+                    </div>
+                    
                 </div>
             </div>
         </main>
