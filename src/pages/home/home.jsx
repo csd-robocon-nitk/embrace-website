6423e4a89597d902fd9344260f063cb3976167f6
Testing wala User <apoorva3569@gmail.com>
2023-10-07 13:40:40 +0530

    Modified header and cover

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,7 +3,21 @@ import s from './home.module.css'
 export default function Home () {
     return (
         <main className = {s.home}>
-            <h1>This is the Home Page</h1>
+            <div className = {s.cover}>
+                <div className = {s.content}>
+                    <h1>EMBRACE</h1><br />
+                    <h2>
+                        <span>E</span>nabling
+                        <span> M</span>ulti-disciplinary and
+                        <span> B</span>roader
+                        <span> R</span>adio
+                        <span> A</span>pplications for
+                        <span> C</span>ommunity
+                        <span> E</span>mpowerment
+                    </h2><br />
+                    {/* <h3>Empowering Communities through Disaster Resilience and Education</h3> */}
+                </div>
+            </div>
         </main>
     )
 }
