9018797c6240ea5e0103eed0ce14e7c928beead9
programmer-er <jhariharan2004@gmail.com>
2023-10-09 22:49:19 +0530

    Added responsivity to homepage

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -5,6 +5,7 @@ export default function Home () {
         <main className = {s.home}>
             <div className = {s.cover}>
                 <img className = {s.antenna} src="/antenna.png" alt="" />
+                <img className = {s.illustration} src="/illustration-blue.png" alt="" />
                 <div className = {s.content}>
                     <div>
                         <h1>EMBRACE</h1><br />
@@ -19,7 +20,6 @@ export default function Home () {
                         </h2>
                     </div>
                     {/* <h3>Empowering Communities through Disaster Resilience and Education</h3> */}
-                    <img src="/illustration-blue.png" alt="" />
                     
                 </div>
             </div>
