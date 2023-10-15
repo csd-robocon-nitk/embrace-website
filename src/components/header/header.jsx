236b6c942e2ec93924926afb26b9cdf688323a9a
programmer-er <jhariharan2004@gmail.com>
2023-10-15 16:06:47 +0530

    Changes to header - current page indicated in navbar - sub-menu for resources

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,58 +1,87 @@
 import { useRef, useState } from 'react'
 import './header.css'
 import { useEffect } from 'react'
+import { Link, Navigate, useLocation, useNavigate } from 'react-router-dom'
 
-export default function Header () {
-    let [navbarOpen, setNavbarOpen] = useState(false)
-    let navRef = useRef()
+export default function Header() {
+	let [navbarOpen, setNavbarOpen] = useState(false)
+	let path = useLocation().pathname
+	let navigate = useNavigate()
 
-    useEffect(() => {
-        document.addEventListener("click", () => setNavbarOpen(false))
-        navRef.current.addEventListener("click", (e) => e.stopPropogation())
-    }, [])
+	useEffect(() => {
+		window.scrollTo(0, 0)
+	}, [path])
 
-    function openNav (e) {
-        if (!navbarOpen) {
-            e.stopPropagation()
-            setNavbarOpen(true)
-        }
-    }
+	useEffect(() => {
+		document.addEventListener('click', () => setNavbarOpen(false))
+	}, [])
 
-    // let headerRef = useRef()
+	function openNav(e) {
+		if (!navbarOpen) {
+			e.stopPropagation()
+			setNavbarOpen(true)
+		}
+	}
 
-    // useEffect(() => {
+	// let headerRef = useRef()
 
-    //     let y = window.scrollY
-    //     if (y > 60) headerRef.current.className = "bg"
-    //     else headerRef.current.className = ""
+	// useEffect(() => {
 
-    //     document.addEventListener("scroll", e => {
-    //         let y = window.scrollY
-    //         if (y > 60) headerRef.current.className = "bg"
-    //         else headerRef.current.className = "bg"
-    //     })
-    // }, [])
-    
-    return (
-        <header className={navbarOpen ? "navbar-open" : ""} /* ref = {headerRef} */>
+	//     let y = window.scrollY
+	//     if (y > 60) headerRef.current.className = "bg"
+	//     else headerRef.current.className = ""
 
-            <div className="content">
-                <div className="left">
-                    <img src="/ham.svg" alt="" tabIndex={0} onClick = {openNav}/>
-                    <img src="/logo.png" alt="" id = "logo" />
-                </div>
-                <nav ref = {navRef}>
-                    <ul>
-                        <li><a href="/">HOME</a></li>
-                        <li><a href="/aboutus">ABOUT US</a></li>
-                        <li><a href="/centres">NODAL CENTERS</a></li>
-                        <li><a href="/resources">RESOURCES</a></li>
-                        <li><a href="/involve">GET INVOLVED</a></li>
-                        <li><a href="/gallery">GALLERY</a></li>
-                        <li><a href="/contact">CONTACT</a></li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
-    )
+	//     document.addEventListener("scroll", e => {
+	//         let y = window.scrollY
+	//         if (y > 60) headerRef.current.className = "bg"
+	//         else headerRef.current.className = "bg"
+	//     })
+	// }, [])
+
+	return (
+		<header
+			className={navbarOpen ? 'navbar-open' : ''} /* ref = {headerRef} */
+		>
+			<div className="content">
+				<div className="left">
+					<img src="/ham.svg" alt="" tabIndex={0} onClick={openNav} />
+					<img src="/logo.png" alt="" id="logo" />
+				</div>
+				<nav>
+					<ul>
+						<li className={(path == '/' ? 'active' : '')}>
+							<Link to="/">HOME</Link>
+						</li>
+						<li className={(path == '/aboutus' ? 'active' : '')}>
+							<Link to="/aboutus">ABOUT US</Link>
+						</li>
+						<li className={(path == '/centres' ? 'active' : '')}>
+							<Link to="/centres">NODAL CENTERS</Link>
+						</li>
+						<li className={(path == '/resources' ? 'active' : '')}>
+							<a onClick = {() => window.innerWidth <= 920 && navigate("/resources")}>
+								RESOURCES
+								<div>
+									<div>
+										<Link to="/resources#course">Course Materials</Link>
+										<Link to="/resources#simulations">Lab Simulations</Link>
+										<Link to="/resources#blog">Blog</Link>
+									</div>
+								</div>
+							</a>
+						</li>
+						<li className={(path == '/involve' ? 'active' : '')}>
+							<Link to="/involve">GET INVOLVED</Link>
+						</li>
+						<li className={(path == '/gallery' ? 'active' : '')}>
+							<Link to="/gallery">GALLERY</Link>
+						</li>
+						<li className={(path == '/contact' ? 'active' : '')}>
+							<Link to="/contact">CONTACT</Link>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</header>
+	)
 }
